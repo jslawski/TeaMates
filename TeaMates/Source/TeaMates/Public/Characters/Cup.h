99701b34d029b87581ca606c59f0da330093cb7d
https://gapp-git.eng.utah.edu/u1306392 <u1306392.umail.Utah.edu>
2021-11-17 23:17:42 -0700

    Individual cup and pot movement

--- TeaMates/Source/TeaMates/Public/Characters/Cup.h
+++ TeaMates/Source/TeaMates/Public/Characters/Cup.h
@@ -4,33 +4,33 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "Pot.generated.h"
+#include "Cup.generated.h"
 
 class USpringArmComponent;
 class UCameraComponent;
 class UStaticMeshComponent;
 
 UCLASS()
-class TEAMATES_API APot : public ACharacter
+class TEAMATES_API ACup : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this character's properties
-	APot();
+	ACup();
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-    USpringArmComponent* springArmComp;
+        USpringArmComponent* springArmComp;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-    UCameraComponent* cameraComp;
+        UCameraComponent* cameraComp;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-    UStaticMeshComponent* meshComp;
+        UStaticMeshComponent* meshComp;
 
     void MoveHorizontal(float value);
     void MoveVertical(float value);
-	
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
